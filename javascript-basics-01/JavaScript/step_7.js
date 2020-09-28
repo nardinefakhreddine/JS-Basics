1653d74ccb83806c1c1aab1daefed74e27804e5e
Nardine <nardinefakhreddine@gmail.com>
2020-09-28 13:25:23 +0300

    step_7

---
+++ javascript-basics-01/JavaScript/step_7.js
@@ -0,0 +1,14 @@
+var shoe_size=prompt("enter you shoe size");
+var birth_year=prompt("enter you birth year");
+
+function stp_7(shoe_size , birth_year){
+var res1=(shoe_size*2)+5;
+var res2=res1*50-birth_year;
+var res=res2+1766;
+alert(res);
+
+
+
+
+}
+stp_7(shoe_size , birth_year);
