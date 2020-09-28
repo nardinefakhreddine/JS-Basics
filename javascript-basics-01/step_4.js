d12bb4c6572eb1aa45d218df3a4c8a270ef09952
Nardine <nardinefakhreddine@gmail.com>
2020-09-28 12:26:49 +0300

    step-4

---
+++ javascript-basics-01/step_4.js
@@ -0,0 +1,6 @@
+function val(){
+var name=document.getElementById("name").value;
+var surname=document.getElementById("surname").value;
+var city=document.getElementById("city").value;
+alert("Hello \n"+"name: "+name+"\n"+"Surname: "+surname+"\n"+"city "+city);
+}
