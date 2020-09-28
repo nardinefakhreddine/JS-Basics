beb98230bb38f69864460b2ebf1598c4afc3e25a
Nardine <nardinefakhreddine@gmail.com>
2020-09-28 13:35:02 +0300

    step_8

---
+++ javascript-basics-01/JavaScript/step_8.js
@@ -0,0 +1,11 @@
+var age=prompt("enter your age");
+if(age>18){
+alert("you are over 18");
+}
+
+if(age==0){
+
+var age=prompt("reenter your age over 0");
+}else{
+    alert("you are under 18");
+    }
