59ae64e6b3d59760fe980ee2bb98a175b9557be7
Nardine <nardinefakhreddine@gmail.com>
2020-09-28 12:57:34 +0300

    step-6

---
+++ javascript-basics-01/JavaScript/step_6.js
@@ -0,0 +1,11 @@
+var firstnumber=prompt("enter your first nb:");
+var secondnumber=prompt("enter your second nb:");
+
+alert(firstnumber/secondnumber);
+function divide(){
+    var firstnumbr=document.getElementById("first_number").value;
+    var secondnumbr=document.getElementById("second_number").value;
+
+    alert(firstnumbr/secondnumbr);
+
+}
