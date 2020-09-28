7902e85d55df002f78d13896c32fac6368da0fd0
Nardine <nardinefakhreddine@gmail.com>
2020-09-28 12:46:51 +0300

    step-5

---
+++ javascript-basics-01/step_5.js
@@ -0,0 +1,12 @@
+var firstnumber=prompt("enter your first nb:");
+var secondnumber=prompt("enter your second nb:");
+
+alert("multiply"+firstnumber*secondnumber);
+function multiple(){
+    var firstnumbr=document.getElementById("first_number").value;
+    var secondnumbr=document.getElementById("second_number").value;
+
+    alert(firstnumbr*secondnumbr);
+
+}
+
