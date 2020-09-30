f9d652d06cbb7ecc99a3c7ffc5c16f124532fa0d
Nardine <nardinefakhreddine@gmail.com>
2020-09-30 09:12:09 +0300

    step-1

---
+++ javascript-basics-02/step-1.js
@@ -0,0 +1,9 @@
+var img = document.querySelector("img");
+
+img.addEventListener("mouseover", function(){
+    img.src = "images/image1_2.jpg"
+});
+
+img.addEventListener("mouseout", function(){
+    img.src = "images/image1.jpg"
+});
