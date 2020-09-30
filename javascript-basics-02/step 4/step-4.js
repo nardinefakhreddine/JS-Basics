0efe49213fda9375860ce00064eac3026f5c40b3
Nardine <nardinefakhreddine@gmail.com>
2020-09-30 14:45:22 +0300

    step-4

---
+++ javascript-basics-02/step 4/step-4.js
@@ -0,0 +1,7 @@
+document.querySelector("button").addEventListener("click",myFunction);
+function myFunction(){
+    document.querySelector("#name").value=" ";
+    document.querySelector("#surname").value =" ";
+    document.querySelector("#name").value=" ";
+    document.querySelector("#city").value =" ";
+}
